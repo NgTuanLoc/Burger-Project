f5bdc54e408cbfc4c9ed256ff3058628f232d5a7
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-04 18:54:28 +0700

    Builder Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,20 +1,59 @@
 import React from "react";
 import classes from "./Burger.css";
-import BurgerIngredient from './BurgerIngredients/BurgerIngredients'
+import BurgerIngredient from "./BurgerIngredients/BurgerIngredients";
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients).map(igKey=>{
-        return [...Array(props.ingredients[igKey])].map((_, i)=>{
-             return <BurgerIngredient key={igKey+i} type={igKey}/>
-        })
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
     })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+
+  console.log(transformedIngredients.length);
+    if (transformedIngredients.length===0){
+        transformedIngredients = <p>Please start adding ingredients to your Burger</p>
+    }
   return (
-      <div className={classes.Burger}>
-          <BurgerIngredient type="bread-top"/>
-          {transformedIngredients}
-          <BurgerIngredient type="bread-bottom"/>
-      </div>
-  )
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
 };
 
 export default burger;
+
+// import React from 'react';
+
+// import classes from './Burger.css';
+// import BurgerIngredient from './BurgerIngredients/BurgerIngredients';
+
+// const burger = ( props ) => {
+//     let transformedIngredients = Object.keys( props.ingredients )
+//         .map( igKey => {
+//             return [...Array( props.ingredients[igKey] )].map( ( _, i ) => {
+//                 return <BurgerIngredient key={igKey + i} type={igKey} />;
+//             } );
+//         } )
+//         .reduce((arr, el) => {
+//             return arr.concat(el)
+//         }, []);
+//     console.log(transformedIngredients)
+//     if (transformedIngredients.length === 0) {
+//         transformedIngredients = <p>Please start adding ingredients!</p>;
+//     }
+//     return (
+//         <div className={classes.Burger}>
+//             <BurgerIngredient type="bread-top" />
+//             {transformedIngredients}
+//             <BurgerIngredient type="bread-bottom" />
+//         </div>
+//     );
+// };
+
+// export default burger;
