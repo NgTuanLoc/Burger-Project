be1486ed02571391385de071345791c4ec5aa7ff
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-07 15:11:10 +0700

    Order summery backdrop button ...

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
+import Modal from "../../components/UI/Modal/Modal";
+import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
 const INGREDIENTS_PRICE = {
   salad: 0.5,
@@ -19,6 +21,35 @@ class BurgerBuilder extends Component {
       cheese: 0,
     },
     totalPrice: 4,
+    purchaseable: false,
+    purchasing: false,
+  };
+
+  purchaseHandler = () => {
+    this.setState({
+      purchasing: true,
+    });
+  };
+
+  purchaseableHandler(ingredients) {
+    const sum = Object.keys(ingredients)
+      .map((igKey) => {
+        return ingredients[igKey];
+      })
+      .reduce((sum, el) => sum + el, 0);
+    this.setState({
+      purchaseable: sum > 0,
+    });
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false,
+    });
+  };
+
+  purchaseContinueHandler = () => {
+    alert("You click Continue !!!");
   };
 
   addIngredientHandler = (type) => {
@@ -37,12 +68,14 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredient,
       totalPrice: updatedPrice,
     });
+
+    this.purchaseableHandler(updatedIngredient);
   };
 
   removeIngredientHandler = (type) => {
     const currentCount = this.state.ingredients[type];
 
-    if(currentCount <= 0){
+    if (currentCount <= 0) {
       return;
     }
     const updatedCount = currentCount - 1;
@@ -59,22 +92,37 @@ class BurgerBuilder extends Component {
       ingredients: updatedIngredient,
       totalPrice: updatedPrice,
     });
+
+    this.purchaseableHandler(updatedIngredient);
   };
 
   render() {
-    const disabledInfo = {...this.state.ingredients}
+    const disabledInfo = { ...this.state.ingredients };
 
-    for(let key in disabledInfo){
-      disabledInfo[key] = disabledInfo[key] <= 0
+    for (let key in disabledInfo) {
+      disabledInfo[key] = disabledInfo[key] <= 0;
     }
     return (
       <Aux>
+        <Modal
+          show={this.state.purchasing}
+          modalClosed={this.purchaseCancelHandler}
+        >
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCanceld={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+            price={this.state.totalPrice.toFixed(2)}
+          />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientsAdded={this.addIngredientHandler}
           ingredientsRemoved={this.removeIngredientHandler}
-          disabled = {disabledInfo}
-          price = {this.state.totalPrice.toFixed(2)}
+          disabled={disabledInfo}
+          price={this.state.totalPrice.toFixed(2)}
+          purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
         />
       </Aux>
     );
