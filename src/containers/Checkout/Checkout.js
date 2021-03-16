90ebd4f6ab5ca3327fdd86871f37d80d76aedacc
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-16 22:38:25 +0700

    finish order route

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -46,7 +46,7 @@ class Checkout extends Component {
           render={(props) => (
             <ContactData
               ingredients={this.state.ingredients}
-              price={this.state.totalPrice}
+              totalPrice={this.state.totalPrice}
               {...props }
             />
           )}
