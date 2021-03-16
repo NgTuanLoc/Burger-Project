04f4b49c7ec1e609b86ee4e682563de4662de671
NgTuanLoc <18521011@gm.uit.edu.vn>
2021-03-16 12:48:57 +0700

    add order nav link

---
+++ src/components/Order/Order.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const order = () => (
+  <div>
+    <p>Ingredients: salad (1)</p>
+    <p>
+      Price <strong>45 $</strong>
+    </p>
+  </div>
+);
+
+export default order;
